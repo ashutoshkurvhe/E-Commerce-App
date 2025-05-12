fe34380388d2f21bc05a7826ed496f6a918a731c
ashutosh <kurvheashutosh@gmail.com>
2025-05-12 12:36:53 +0530

    E-Commerce App

--- E-Commerce MERN/frontend/src/components/Layout/Hero.jsx
+++ E-Commerce MERN/frontend/src/components/Layout/Hero.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Hero = () => {
     return (
         <section className="relative">
-            <img src={heroImg} alt="Rebbit" className="w-full h-[400px] md:h-[600px] lg:h-[750] object-cover" />
+            <img src={heroImg} alt="Rebbit" className="w-full h-[400px] md:h-[90vh] lg:h-[750] object-cover" />
             <div className="absolute inset-0 bg-opacity-5 flex items-center justify-center">
                 <div className="text-center text-white p-6">
                     <h1 className="text=4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
